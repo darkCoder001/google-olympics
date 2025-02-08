6330bf7dff7ae62310f12390cfb8a6f715806a9b
Anushree Jaiswal <anushreejaiswal.srn@gmail.com>
2025-02-08 08:32:46 +0530

    updated landing page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,39 @@
 import { About } from "@/components/About/About";
 import Hero from "@/components/Hero/Hero";
+import { Landing } from "@/components/Hero/Landing";
 import { FocusCards as Roadmap } from "@/components/ui/focus-cards";
 
-
 const roadmapCards = [
-  {
-    title: "Level 1",
-    src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
-  },
-  {
-    title: "Level 2",
-    src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
-  },
-  {
-    title: "Level 3",
-    src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
-  },
+	{
+		title: "Level 1",
+		src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
+	},
+	{
+		title: "Level 2",
+		src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
+	},
+	{
+		title: "Level 3",
+		src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
+	},
 
-  {
-    title: "Level 4",
-    src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
-  },
-  {
-    title: "Level 5",
-    src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
-  },
+	{
+		title: "Level 4",
+		src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
+	},
+	{
+		title: "Level 5",
+		src: "https://www.icegif.com/wp-content/uploads/2023/01/icegif-162.gif",
+	},
 ];
 
-
 export default function Home() {
-  return (
-    <div className="h-screen bg-black">
-      <Hero />
-      {/* <About />
-      <Roadmap cards={roadmapCards} />; */}
-    </div>
-  );
+	return (
+		<div className="h-screen w-screen">
+			<Landing />
+			{/*<Hero />
+	  		<About />
+      		<Roadmap cards={roadmapCards} />; */}
+		</div>
+	);
 }
